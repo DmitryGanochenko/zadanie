f16a830fd48655712fd1d1975b8f38fe68a5e887
DmitryGanochenko <dima@dima-HP-15-Notebook-PC>
2017-12-13 22:07:16 +0300

    start

---
+++ files/index1.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>Test1 </title>
+ </head>
+ <body>
+ <?php echo '<p>Привет, test1.local!</p>'; ?>
+ </body>
+</html>
---
+++ files/index2.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>Test2 </title>
+ </head>
+ <body>
+ <?php echo '<p>Привет, test2.local!</p>'; ?>
+ </body>
+</html>
